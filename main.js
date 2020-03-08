9ae75227ccd1d382b38f92e9e1aff0a898fd035c
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-08 23:51:05 +0100

    set clean good password

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 const input = document.getElementById('pass');
 const div = document.querySelector('.message');
 const password = "user";
-const message = "wyjechałam na zawsze"
+const message = "wyjechałam na zwasze";
 
 input.addEventListener('input', (e) => {
- // console.log(e.target.value);
- if (password === e.target.value) {
-  div.textContent = message;
-  e.target.value = '';
- } else {
-  div.textContent = '';
- }
-})
+    // console.log(e.target.value);
+    if (password === e.target.value) {
+        div.textContent = message;
+        e.target.value = '';
+    }
+    else {
+        div.textContent = "";
+    }
 
-input.addEventListener('focus', (e) => {
- e.target.classList.add('active');
-})
-
-input.addEventListener('blur', (e) => {
- e.target.classList.remove('active');
 })
